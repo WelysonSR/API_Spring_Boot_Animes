48ae0daacfc01270a3d2c15578d319f0a51107a5
Welyson Rosa <welysonsr@gmail.com>
2023-02-24 16:06:12 -0300

    feat: metode post anime developed

--- src/main/java/inicial/spring/boot/springboote/servece/AnimeService.java
+++ src/main/java/inicial/spring/boot/springboote/servece/AnimeService.java
@@ -5,12 +5,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-  private List<Anime> animes = List.of(new Anime(1L,"DBZ"), new Anime(2L,"Kengan Ashura"));
-
+  private static List<Anime> animes;
+  static {
+    animes = new ArrayList<>(List.of(new Anime(1L,"DBZ"), new Anime(2L,"Kengan Ashura")));
+  }
   public List<Anime> listAll() {
     return animes;
   }
@@ -18,5 +22,11 @@ public class AnimeService {
   public Anime findById(long id) {
     return animes.stream().filter(anime -> anime.getId().equals(id)).findFirst()
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+  }
+
+  public Anime save(Anime anime) {
+    anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+    animes.add(anime);
+    return anime;
   }
 }
